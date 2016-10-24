cde27751b248c5455fb3123b151f7c7579e73cc6
Brian Lopez <email@brianlopez.us>
2016-10-24 12:12:36 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,22 +5,22 @@ set-alias -name wr -value write-verbose
 wr "Installing nuget"
 (new-object net.webclient).downloadfile("https://nuget.org/nuget.exe","$env:userprofile\nuget.exe")
 
-wr "add relevent sources"
-
-wr "install packages"
+wr "Set the alias"
 set-alias -name nuget -value "$env:userprofile\nuget.exe"
 
+wr "Nuget Update Self
+nuget update -self
+
+wr "add relevent sources"
+nuget sources add -name "myget" -source "https://www.myget.org/F/thebrianlopez/api/v2" -verbosity detailed
+
+wr "Configure default path for packages"
+nuget config -set repositoryPath=Programs
+
 wr "nuget list sources"
+nuget sources list
 
 wr "nuget disable sources"
-
-wr "nuget add custom sources"
-iex "nuget sources add -name myget -source https://www.myget.org/F/thebrianlopez/api/v2"
-
-wr "nuget install packages to default path"
-
-wr "nuget config"
-iex "nuget config -set repositoryPath=Programs"
+nuget install axsadministration -o $env:userprofile\downloads -excludeversion
 
 wr "Complete"
-iex "nuget install axsadministration -o $env:userprofile\downloads -excludeversion"
